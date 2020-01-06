6ed00f21c3a655849ea69595784f372447f296a5
ximuli <ximuli666@163.com>
2020-01-06 15:26:35 +0800

    重写首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,28 +8,6 @@ Page({
     query: '',
     result: ''
   },
-  onInput: function(e) {
-    this.setData({ query: e.detail.value })
-    if (this.data.query.length > 0) {
-      this.setData({ isHidden: false })
-    }else {
-      this.setData({ isHidden: true })
-    }
-  },
-  emptyInput: function() {
-    this.setData({ isHidden: true, query: '' })
-  },
-  onConfirm: function() {
-    if (!this.data.query) return 
-    translate(this.data.query, 'auto', this.data.targetLang.lang).then(res => {
-      this.setData({ result: res.trans_result[0].dst })
-
-      let history = wx.getStorageSync('history') || []
-      history.unshift({ query: this.data.query, result: this.data.result })
-      history.length > 10 ? 10 : history.length //最多保存 10 条
-      wx.setStorageSync('history', history)
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -41,10 +19,11 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    if (this.data.targetLang.lang !== app.languageData.selectLang.lang) {
-      this.setData({ targetLang: app.languageData.selectLang })
-      this.onConfirm()  //切换语言后自动翻译  
-    } 
+  onShow: function () {},
+  goToTranslation() {
+    console.log(666)
+    wx.navigateTo({
+      url: '/packageTranslation/pages/index/index'
+    })
   }
 })
