ae172615df76a60f9454b9076f82afdb6c295804
ximuli <ximuli666@163.com>
2020-01-08 17:18:13 +0800

    为一言模块添加了动画

--- packageYiyan/pages/index/index.js
+++ packageYiyan/pages/index/index.js
@@ -2,35 +2,60 @@ const app = getApp()
 
 Page({
   data: {
-    hitokoto: '在季节的车上，如果你要提前下车，请别推醒装睡的我，这样我可以沉睡到终点，假装不知道你已经离开。'
+    hitokoto: ''
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if(options.query) {
-      this.setData({ query: options.query })
-    }
+    this.getYiyan()
   },
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    this.getYiyan()
-  },
+  onShow: function () {},
   getYiyan() {
     const self = this
     wx.request({
-      url: 'https://v1.hitokoto.cn',
+      url: 'https://v1.hitokoto.cn///',
       success(res) {
         const { hitokoto } = res.data
+        if (hitokoto) {
+          self.setData({
+            hitokoto
+          })
+        } else {
+          self.setData({
+            hitokoto: '在季节的车上，如果你要提前下车，请别推醒装睡的我，这样我可以沉睡到终点，假装不知道你已经离开。'
+          })
+        }
+      },
+      fail(err) {
         self.setData({
-          hitokoto
+          hitokoto: '在季节的车上，如果你要提前下车，请别推醒装睡的我，这样我可以沉睡到终点，假装不知道你已经离开。'
         })
       }
     })
   },
-  onNext() {
-    this.getYiyan()
+  onNext(e) {
+    // 动画
+    this.animate('.wrapper', [
+      { opacity: 1, translateX: '0' },
+      { opacity: 0, translateX: '-10%' },
+      { opacity: 0, translateX: '10%' },
+      { opacity: 1, translateX: '0' },
+    ], 600, function () {
+        this.clearAnimation('.wrapper', { opacity: true, translateX: true }, function () {
+          console.log("清除了 .wrapper 上的 opacity 和 translateX 属性")
+      })
+    }.bind(this))
+
+    this.asyncUpdateYiyan()
+  },
+  asyncUpdateYiyan() {
+    const timerId = setTimeout(() => {
+      this.getYiyan()
+      clearTimeout(timerId)
+    }, 300)
   }
 })
